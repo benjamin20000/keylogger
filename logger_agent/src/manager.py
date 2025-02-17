fcdd97162101fc34fb97faa78f31f3e55a63822f
benjamin <2000beni770@gmail.com>
2025-02-17 18:11:42 +0200

    coneccting manager for writing to file class

--- logger_agent/src/manager.py
+++ logger_agent/src/manager.py
@@ -3,7 +3,6 @@ from src.writer.console_log import ConsoleLog
 from src.writer.file_log import FileLog 
 from src.parser.encryption.xor import Xor
 
-
 from time import sleep
 from threading import Thread
 
@@ -13,13 +12,15 @@ class manager:
     def __init__(self):
         self.l = Listener()
         self.wr = ConsoleLog()
+        self.wr2 = FileLog()
 
     def write_buffer(self):
         while True:
-            print("Executing function...")
-            a = self.l.get_buffer()
-            self.wr.write(a)
-            sleep(10)  # TODO add the sleep time to config file  
+            if self.l.buffer_has_data():
+                buffer = self.l.get_buffer()
+                self.wr.write(buffer)
+                self.wr2.write(buffer)
+            sleep(5)   #TODO add the sleep time to config file  
 
     def main(self):
         ## write
