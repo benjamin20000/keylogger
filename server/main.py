fc51c1aaac230b7e03285b2cb5a5f06291c56043
benjamin <2000beni770@gmail.com>
2025-02-20 23:59:26 +0200

    change verb

--- server/main.py
+++ server/main.py
@@ -13,17 +13,17 @@ def create_json():
 def get_data():
     if not os.path.exists(json_path):
         create_json()
-    new_data = json.loads(request.data.decode("utf-8"))
+    incoming_json = json.loads(request.data.decode("utf-8"))
     with open(json_path, "r+") as f: 
         current_json = json.load(f)
-        if new_data["mac_addres"] not in current_json:            
-            current_json[new_data["mac_addres"]] = new_data["data"]
+        if incoming_json["mac_addres"] not in current_json:            
+            current_json[incoming_json["mac_addres"]] = incoming_json["data"]
         else:
-            current_json[new_data["mac_addres"]] += new_data["data"]
+            current_json[incoming_json["mac_addres"]] += incoming_json["data"]
         f.seek(0)
         json.dump(current_json, f, indent=2)
+        
              
-    print(new_data["mac_addres"])
     return Response('We recieved something…')
 
 if __name__ == '__main__':
