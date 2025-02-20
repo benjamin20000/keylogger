d1da64dbea790a6aa2fa17860d5498e0a2ddebb7
Yoel ider <yoeli5746@gmail.com>
2025-02-20 15:46:27 +0200

    change server

--- server/main.py
+++ server/main.py
@@ -4,18 +4,20 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def get_data():
-    try:
-        with open("server_data.json", "r") as f:
-                data = json.load(f) 
-    except (FileNotFoundError, json.JSONDecodeError):
-        data = []
-            
-    data.append(json.loads(request.data.decode("utf-8")))
-    with open("server_data.json","w") as f:
-            json.dump(data, f, indent=2)
-
-    print('Recieved data: {}'.format(request.data))
+    print(json.loads(request.data.decode("utf-8")))
     return Response('We recieved something…')
+    # try:
+    #     with open("server_data.json", "r") as f:
+    #             data = json.load(f) 
+    # except (FileNotFoundError, json.JSONDecodeError):
+    #     data = []
+            
+    # data.append(json.loads(request.data.decode("utf-8")))
+    # with open("server_data.json","w") as f:
+    #         json.dump(data, f, indent=2)
+
+    # print('Recieved data: {}'.format(request.data))
+    # return Response('We recieved something…')
 
 if __name__ == '__main__':
     app.run(debug=True)
